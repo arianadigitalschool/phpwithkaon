a4db99b871e5e81213882b2a88e3601848cb2149
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2025-03-19 19:19:22 +0100

    loops and conditionals

---
+++ day2/index.php
@@ -0,0 +1,46 @@
+<?php
+
+    $num = 0;
+
+    if($num<0){
+        echo "$num is less than zero";
+    }elseif($num==0){
+        echo "$num is equal to zero";
+    }
+    else{
+        echo "$num is bigger than zero";
+    }
+
+    $age = -13;
+
+    if(($age>12) && ($age<20)){
+        echo "<br> You are a teenager <br>";
+    }
+
+    switch($age){
+        case($age >=0 && $age < 18);
+            echo "You are a minor <br>";
+            break;
+        case($age >=18 && $age <= 25);
+            echo "You are an adult <br> ";
+            break;
+        case($age >25);
+            echo "You are a big adult <br>";
+            break;
+        default:
+            echo " <br> Invalid age input <br>";
+            break;
+    }
+
+    $y = 6; // 1+1=2 // 3 // 4 // 5 // 6 //
+    // while($y <= 6){
+    //     echo "<br> The number is: $y <br>";
+
+    //     $y++;
+    // };
+
+    do{
+        echo "The number is $y <br>";
+        $y++;
+    }while($y<=5);
+?>
